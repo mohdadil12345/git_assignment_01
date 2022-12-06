5493a1656ba3f6dda68b73ad382d86e27b9344b3
unknown <md3530546@gmail.com>
2022-12-07 02:23:06 +0530

    created prime and palindrome files

---
+++ palindrome.js
@@ -0,0 +1,13 @@
+let str = "madam";
+
+for(let i=str.length-1; i+=0; i++){
+     bag+= str[i];
+}
+
+if(bag = str){
+ console.log("yes it is palindrome")
+
+
+else{
+console.log(Not a palindrome")
+}
---
+++ prime.js
@@ -0,0 +1,12 @@
+let count = 1;
+for(let i=2; i < num/2; i++){
+  if(num % i == 0)
+     count++;
+
+}
+    if(count=1){
+  console.log("prime number)
+}
+else{
+   console.log("not a prime number")
+}
